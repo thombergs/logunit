1de69a743f59c8f5a28a881c65c00bfe750676b5
Tom Hombergs <tom.hombergs@gmail.com>
2013-09-09 22:53:16 +0200

    added licence notice created javadoc prepared release 1.0

--- logunit-core/src/main/java/org/wickedsource/logunit/Expectation.java
+++ logunit-core/src/main/java/org/wickedsource/logunit/Expectation.java
@@ -1,3 +1,17 @@
+/**
+ *   Copyright 2013 Tom Hombergs (tom.hombergs@gmail.com)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package org.wickedsource.logunit;
 
 import java.util.regex.Matcher;
@@ -7,7 +21,7 @@ import java.util.regex.Pattern;
  * A LogUnit expectation defines what is expected to be logged during a unit
  * test.
  * 
- * @author Tom
+ * @author Tom Hombergs <tom.hombergs@gmail.com>
  * 
  */
 public class Expectation {
